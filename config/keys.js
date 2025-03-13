ed9f9cac4971e74b9697906ed37d36ce895d9c55
AmitRawat01 <amitrawat8774@gmail.com>
2025-03-13 17:28:20 +0530

    updated

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-    jwtSecret: 'yourSecretKey',
-    sendGridApiKey: 'yourSendGridApiKey'
-  };
-  
+
+require('dotenv').config();
+
+export const jwtSecret = process.env.JWT_SECRET;
+export const sendGridApiKey = process.env.SENDGRID_API_KEY;
