33d8b87cf4ec1f6b974bbd3f90f451a6cb454451
AmitRawat01 <amitrawat8774@gmail.com>
2025-03-19 10:50:52 +0530

    API Test

--- src/routes/flipkartRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { fetchFlipkartMobiles, fetchFlipkartMobilesFull } = require('../controllers/flipkartController');
-
-const router = express.Router();
-
-router.get('/mobile', fetchFlipkartMobiles);
-router.get('/mobile/full', fetchFlipkartMobilesFull);
-
-module.exports = router;
--- src/routes/snapdealRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { fetchSnapdealTShirts } = require('../controllers/snapdealController');
-
-const router = express.Router();
-
-router.get('/t-shirt', fetchSnapdealTShirts);
-
-module.exports = router;
